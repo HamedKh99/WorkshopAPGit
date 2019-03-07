9505d7f35f91b198b6ab5eb644a79dc7fab4fa6c
HamedKh99 <goo.hamedacc@gmail.com>
2019-03-07 11:26:15 +0330

    changed operations and nums main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int x = 15;
+        int a = 17;
+        System.out.println(a);
         int y = 80;
         int z = 10;
         System.out.println(x + y + z * 2);
+        System.out.println(x * 2);
     }
 }
