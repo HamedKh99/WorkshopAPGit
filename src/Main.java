c8eff4a91012555a0649a2e0ed4a962dab81271d
kiarash <krsam99@gmail.com>
2019-03-07 10:52:15 +0330

    change number

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int x = 15;
-        int y = 20;
+        int y = 9;
         int z = 10;
         System.out.println(x + y + z);
     }
