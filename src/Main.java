f2999a907e683cdd6164db6ab322dad2d8ddcd31
HamedKh99 <goo.hamedacc@gmail.com>
2019-03-07 11:23:58 +0330

    changed operations main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int x = 15;
         int y = 80;
-        int z = 10;
-        System.out.println(x + y + z * 2);
+        int z = 20;
+        System.out.println(x + y + z / 2);
     }
 }
