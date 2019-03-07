ce542ab45e80a07a91cb00eecb2f8b0121d42c26
HamedKh99 <goo.hamedacc@gmail.com>
2019-03-07 10:52:29 +0330

    changed main 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int x = 15;
-        int y = 20;
+        int y = 80;
         int z = 10;
-        System.out.println(x + y + z);
+        System.out.println(x + y + z * 2);
     }
 }
